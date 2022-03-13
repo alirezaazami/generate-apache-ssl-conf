f1da810a214f06e9a047b592e6328c8febee330a
alirezaazami <31056961+alirezaazami@users.noreply.github.com>
2022-03-13 23:01:40 +0330

    add need comment to host file

--- create_new_hosts.sh
+++ create_new_hosts.sh
@@ -2,5 +2,14 @@
 
 
 start_line=$(grep -n '#startweb' /etc/hosts | cut -d: -f 1)
-start_line=$(($start_line+1))
-sudo sed -iz "${start_line}s/.*/${1}/" /etc/hosts
+if [ $start_line=='' ];then
+	sudo echo "#startweb" >> /etc/hosts
+	sudo echo ${1} >> /etc/hosts
+	sudo echo "#endweb" >> /etc/hosts
+else
+	echo 'no'
+	exit 0
+	start_line=$(($start_line+1))
+	sudo sed -iz "${start_line}s/.*/${1}/" /etc/hosts
+	
+fi
